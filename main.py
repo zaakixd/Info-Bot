d70bafcdecdc19d15420b90f4d814b888b1ae78d
ZaakiXD! <zaakixd@gmail.com>
2026-03-17 22:52:25 +0530

    Refactor user info command and improve embed
    Updated user information retrieval and embed structure.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ TOKEN = os.environ.get("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True
-intents.presences = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -15,7 +14,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    activity = discord.Activity(type=discord.ActivityType.listening, name="KK On Low !")
+    activity = discord.Activity(type=discord.ActivityType.listening, name="KK On lowwW !")
     await bot.change_presence(activity=activity)
     print(f"Logged in as {bot.user}")
 
@@ -24,55 +23,42 @@ async def on_ready():
 async def info(ctx, user_id: int):
     try:
         user = await bot.fetch_user(user_id)
-        member = ctx.guild.get_member(user_id)
 
         embed = discord.Embed(
-            title="Discord User Lookup",
-            color=discord.Color.blue(),
+            title="User Information",
+            color=discord.Color.from_rgb(255,255,255),  # white border
             timestamp=datetime.utcnow()
         )
 
-        # Avatar
         embed.set_thumbnail(url=user.display_avatar.url)
 
-        # Username
-        embed.add_field(name="Username", value=user.name, inline=True)
-
-        # Global name
-        embed.add_field(name="Display Name", value=user.global_name, inline=True)
-
-        # Tag
-        embed.add_field(name="Tag", value=f"{user}", inline=True)
-
-        # User ID
-        embed.add_field(name="User ID", value=user.id, inline=False)
-
-        # Status (only if in server)
-        if member:
-            embed.add_field(name="Status", value=str(member.status), inline=True)
-        else:
-            embed.add_field(name="Status", value="Not in this server", inline=True)
-
-        # Avatar link
-        embed.add_field(name="Avatar", value=user.display_avatar.url, inline=False)
-
-        # Banner
-        if user.banner:
-            embed.add_field(name="Banner", value=user.banner.url, inline=False)
-            embed.set_image(url=user.banner.url)
-        else:
-            embed.add_field(name="Banner", value="No banner", inline=False)
-
-        # Nitro guess
-        nitro = "Unknown"
-        if user.banner or user.display_avatar.is_animated():
-            nitro = "Possible"
-
-        embed.add_field(name="Nitro", value=nitro, inline=True)
+        embed.add_field(
+            name="Username",
+            value=user.name,
+            inline=True
+        )
 
         embed.add_field(
-            name="Profile Link",
-            value=f"https://discord.com/users/{user.id}",
+            name="Display Name",
+            value=user.global_name if user.global_name else "None",
+            inline=True
+        )
+
+        embed.add_field(
+            name="User ID",
+            value=user.id,
+            inline=False
+        )
+
+        embed.add_field(
+            name="Account Created",
+            value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"),
+            inline=False
+        )
+
+        embed.add_field(
+            name="Profile Picture",
+            value=user.display_avatar.url,
             inline=False
         )
 
@@ -81,7 +67,7 @@ async def info(ctx, user_id: int):
         await ctx.send(embed=embed)
 
     except:
-        await ctx.send("User not found or invalid ID.")
+        await ctx.send("Invalid user ID or user not found.")
 
 
 bot.run(TOKEN)
