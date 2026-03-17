7655e29ccce6ddd162f9cf3db0e883f297f0fb52
ZaakiXD! <zaakixd@gmail.com>
2026-03-17 22:18:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import discord
 from discord.ext import commands
+import os
 
-TOKEN = "MTQ4MzUwMjU0NTk2NzI1MTYyNw.GhwMIQ.yOTZT2sAL-b4qb0j1CpJwBU7PfSk49nwSyjs7E"
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 intents.members = True
+intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -23,17 +26,17 @@ async def info(ctx, user_id: int):
 
         embed.set_thumbnail(url=user.display_avatar.url)
 
-        embed.add_field(name="Username", value=f"{user.name}#{user.discriminator}", inline=True)
+        embed.add_field(name="Username", value=user.name, inline=True)
         embed.add_field(name="User ID", value=user.id, inline=True)
-        embed.add_field(name="Bot", value=user.bot, inline=True)
+        embed.add_field(name="Bot Account", value=user.bot, inline=True)
         embed.add_field(name="Account Created", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=False)
-        embed.add_field(name="Avatar URL", value=user.display_avatar.url, inline=False)
+        embed.add_field(name="Avatar", value=user.display_avatar.url, inline=False)
 
         embed.set_footer(text=f"Requested by {ctx.author}")
 
         await ctx.send(embed=embed)
 
-    except:
+    except Exception as e:
         await ctx.send("User not found or invalid ID.")
 
 bot.run(TOKEN)
